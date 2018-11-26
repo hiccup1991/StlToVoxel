48537257deef5195d7ecb2c69d36cd82699de5eb
hiccup1991 <hiccup1991@hotmail.com>
2018-11-26 22:12:49 +0900

    bug fixed and made beautiful gui

--- Main.py
+++ Main.py
@@ -72,7 +72,10 @@ class MainWindow:
     inputdirname = '/home/dev/Documents/StlToVoxel/examples'
     partcolor = ['red', 'green', 'blue', 'yellow', 'navy', 'gold', 'purple', 'cyan', 'orange', 'azure', 'ivory', 'snow', 'linen', 'cornsilk', 'tomato', 'coral']
     content = Content()
-    content.voxel = list()
+    backgroundColor = '#5ed845'
+    disabledforeground = '#a3a3a3'
+    highlightbackground = '#5eff45'
+
     def __init__(self, top=None):
         '''This class configures and populates the toplevel window.
            top is the toplevel containing window.'''
@@ -83,77 +86,124 @@ class MainWindow:
         _ana2color = '#d9d9d9' # X11 color: 'gray85' 
 
         top.geometry("937x579+650+150")
-        top.title("StlToVoxel")
-        top.configure(highlightcolor="black")
+        top.title("A Cube Box")
+        top.configure(background=self.backgroundColor)
+
+        self.lblLogo = Label(top)
+        self.lblLogo.place(relx=0.06, rely=0.02, height=100, width = 100)  
+        self.original = Image.open('./logo.png')
+        resized = self.original.resize((100, 100),Image.ANTIALIAS)
+        self.image = ImageTk.PhotoImage(resized) # Keep a reference, prevent GC
+        self.lblLogo.configure(image=self.image, background = self.backgroundColor)
+        
+        self.frmInputStl = LabelFrame(top)
+        self.frmInputStl.place(relx=0.02, rely=0.2, relheight=0.1 , relwidth=0.25)
+        self.frmInputStl.configure(relief=RIDGE)
+        self.frmInputStl.configure(text='''Input STL File''')
+        self.frmInputStl.configure(background=self.backgroundColor)
+        
+        self.btnOpen = Button(self.frmInputStl)
+        self.btnOpen.place(relx=0.05, rely=0.05, relheight=0.9, relwidth=0.9)
+        self.btnOpen.configure(text='''Open''')
+        self.btnOpen.bind("<Button-1>", self.open_directory_dialog)
+        self.btnOpen.configure(background=self.backgroundColor)
+        self.btnOpen.configure(disabledforeground=self.disabledforeground)
+
+        self.frmLayerThickness = LabelFrame(top)
+        self.frmLayerThickness.place(relx=0.02, rely=0.35, relheight=0.1 , relwidth=0.25)
+        self.frmLayerThickness.configure(relief=RIDGE)
+        self.frmLayerThickness.configure(text='''Layer Thickness''')
+        self.frmLayerThickness.configure(background=self.backgroundColor)
+
+        self.entLayerThickness = Entry(self.frmLayerThickness)
+        self.entLayerThickness.place(relx=0.05, rely=0.05, relheight=0.75, relwidth=0.9)
+        self.entLayerThickness.insert(0, "0.02")
+        self.entLayerThickness.configure(background=self.backgroundColor)
+        self.entLayerThickness.configure(justify=RIGHT)
+
+        self.frmModify = LabelFrame(top)
+        self.frmModify.place(relx=0.02, rely=0.5, relheight=0.2 , relwidth=0.25)
+        self.frmModify.configure(relief=RIDGE)
+        self.frmModify.configure(text='''Modify Changes''')
+        self.frmModify.configure(background=self.backgroundColor)
+
+        self.btnStlToVoxel = Button(self.frmModify)
+        self.btnStlToVoxel.place(relx=0.05, rely=0.05, relheight=0.35, relwidth=0.9)
+        self.btnStlToVoxel.configure(text='''Convert''')
+        self.btnStlToVoxel.bind("<Button-1>", self.stl_to_voxel_files)
+        self.btnStlToVoxel.configure(background=self.backgroundColor)
+        self.btnStlToVoxel.configure(disabledforeground=self.disabledforeground)
+
+        self.lblPart = Label(self.frmModify)
+        self.lblPart.place(relx=0.05, rely=0.45, relheight=0.2, relwidth=0.45)
+        self.lblPart.configure(text='''Part : ''')
+        self.lblPart.configure(background=self.backgroundColor)
+
+        self.cboPartList = ttk.Combobox(self.frmModify)
+        # self.cboPartList['values'] = ('KJ1.stl', 'KJ2.stl', 'KJ3.stl', 'KJ4.stl', 'KJ5.stl')
+        # self.cboPartList.current(0)
+        self.cboPartList.place(relx=0.5, rely=0.45, relheight=0.2, relwidth=0.45)
+        self.cboPartList.configure(background=self.backgroundColor)
+
+        self.lblPartColor = Label(self.frmModify)
+        self.lblPartColor.place(relx=0.05, rely=0.7, relheight=0.2, relwidth=0.45)
+        self.lblPartColor.configure(text='''Part Color : ''')
+        self.lblPartColor.configure(background=self.backgroundColor)
+
+        self.cboPartColor = ttk.Combobox(self.frmModify)
+        self.cboPartColor.place(relx=0.5, rely=0.7, relheight=0.2, relwidth=0.45)
+        self.cboPartColor['values'] = ('red', 'green', 'blue', 'yellow', 'navy', 'gold', 'purple', 'cyan', 'orange', 'azure', 'ivory', 'snow', 'linen', 'cornsilk', 'tomato', 'coral')
+        self.cboPartColor.bind('<<ComboboxSelected>>', self.on_part_color)
+
+        self.frmMaterial = LabelFrame(top)
+        self.frmMaterial.place(relx=0.02, rely=0.75, relheight=0.08 , relwidth=0.25)
+        self.frmMaterial.configure(relief=RIDGE)
+        self.frmMaterial.configure(text='''Specify Material''')
+        self.frmMaterial.configure(background=self.backgroundColor)
+
+        self.cboMaterial = ttk.Combobox(self.frmMaterial)
+        self.cboMaterial.place(relx=0.05, rely=0.05, relheight=0.75, relwidth=0.9)
+        self.cboMaterial['values'] = ('red', 'green')
+
+        self.frmSaveCancel = LabelFrame(top)
+        self.frmSaveCancel.place(relx=0.02, rely=0.88, relheight=0.08 , relwidth=0.25)
+        self.frmSaveCancel.configure(relief=RIDGE)
+        self.frmSaveCancel.configure(background=self.backgroundColor)
+        
+        self.btnXml = Button(self.frmSaveCancel)
+        self.btnXml.place(relx=0.05, rely=0.05, relheight=0.9, relwidth=0.4)
+        self.btnXml.configure(text='''Save''')
+        self.btnXml.bind("<Button-1>", self.on_xml)
+        self.btnXml.configure(background=self.backgroundColor)
+        self.btnXml.configure(disabledforeground=self.disabledforeground)
+
+        self.btnCancel = Button(self.frmSaveCancel)
+        self.btnCancel.place(relx=0.55, rely=0.05, relheight=0.9, relwidth=0.4)
+        self.btnCancel.configure(text='''Cancel''')
+        self.btnCancel.bind("<Button-1>", self.on_cancel)
+        self.btnCancel.configure(background=self.backgroundColor)
+        self.btnCancel.configure(disabledforeground=self.disabledforeground)
 
         self.cvsVoxelPlayer = Canvas(top)
-        self.cvsVoxelPlayer.place(relx=0.26, rely=0.02, relheight=0.96, relwidth=0.72)   
-        self.cvsVoxelPlayer.configure(background = 'white')
+        self.cvsVoxelPlayer.place(relx=0.28, rely=0.05, relheight=0.91, relwidth=0.70)   
+        self.cvsVoxelPlayer.configure(background = self.backgroundColor)
         self.cvsVoxelPlayer.configure(borderwidth = '4')
         self.cvsVoxelPlayer.configure(relief=RIDGE)
 
         self.fig = Figure(figsize=(60, 60))
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.cvsVoxelPlayer)
-        self.canvas.get_tk_widget().pack()   
+        self.canvas.get_tk_widget().pack(padx='5', pady='5')   
         self.ax = self.fig.add_subplot(111, projection='3d')
-        self.ax.set_ylabel("Y", fontsize=14)
-        self.ax.set_xlabel("X", fontsize=14)
-        self.ax.set_zlabel("z", fontsize=14)
-
-        self.btnOpen = Button(top)
-        self.btnOpen.place(relx=0.02, rely=0.1, height=64, width=207)
-        self.btnOpen.configure(text='''Open''')
-        self.btnOpen.bind("<Button-1>", self.open_directory_dialog)
-
-        self.lblResolution = Label(top)
-        self.lblResolution.place(relx=0.02, rely=0.3, height=34, width=77)
-        self.lblResolution.configure(text='''Resolution : ''')
-
-        self.entResolution = Entry(top)
-        self.entResolution.place(relx=0.10, rely=0.3, height=34, width=130)
-        self.entResolution.insert(0, "50")
-        self.entResolution.configure(width=304)
-
-        self.btnStlToVoxel = Button(top)
-        self.btnStlToVoxel.place(relx=0.02, rely=0.42, height=64, width=207)
-        self.btnStlToVoxel.configure(text='''StlToVoxel''')
-        self.btnStlToVoxel.bind("<Button-1>", self.stl_to_voxel_files)
-
-        self.lblPart = Label(top)
-        self.lblPart.place(relx=0.02, rely=0.6, height=34, width=77)
-        self.lblPart.configure(text='''Part : ''')
-
-        self.cboPartList = ttk.Combobox(top)
-        self.cboPartList['values'] = ('KJ1.stl', 'KJ2.stl', 'KJ3.stl', 'KJ4.stl', 'KJ5.stl')
-        self.cboPartList.current(0)
-        self.cboPartList.place(relx=0.10, rely=0.6, height=34, width=130)
-
-        self.lblPartColor = Label(top)
-        self.lblPartColor.place(relx=0.02, rely=0.72, height=34, width=77)
-        self.lblPartColor.configure(text='''Part Color : ''')
-
-        self.cboPartColor = ttk.Combobox(top)
-        self.cboPartColor.place(relx=0.10, rely=0.72, height=34, width=130)
-        self.cboPartColor['values'] = ('red', 'green', 'blue', 'yellow', 'navy', 'gold', 'purple', 'cyan', 'orange', 'azure', 'ivory', 'snow', 'linen', 'cornsilk', 'tomato', 'coral')
-        self.cboPartColor.configure(text='''Color''')
-        self.cboPartColor.bind('<<ComboboxSelected>>', self.on_part_color)
-
-        self.btnXml = Button(top)
-        self.btnXml.place(relx=0.02, rely=0.82, height=64, width=207)
-        self.btnXml.configure(text='''Xml''')
-        self.btnXml.bind("<Button-1>", self.on_xml)
+        self.ax.set_ylabel("Y", fontsize=12)
+        self.ax.set_xlabel("X", fontsize=12)
+        self.ax.set_zlabel("z", fontsize=12)
 
     def open_directory_dialog(self, event):
         inputdirname = filedialog.askdirectory(initialdir = "./",title = "Select directory")
         if inputdirname:
             self.inputdirname = inputdirname
             print("selected directory: " + inputdirname)
-            filenames = []
-            print("filelist: ")
-            for file in os.listdir(inputdirname):
-                if os.path.isfile(os.path.join(inputdirname, file)):
-                    print(file)
-                    filenames.append(file)
+            filenames = fnmatch.filter(os.listdir(inputdirname), '*.stl')
             filenames.sort()
             self.cboPartList['values'] = filenames
             self.cboPartList.current(0)
@@ -172,7 +222,9 @@ class MainWindow:
                 print("Directory " , outputdirectoryname ,  " already exists")
             outputfilename = outputdirectoryname + "/" + only_name + ".png"
             print("outputfilename: " + outputfilename)
-            convert.doExport(self.inputdirname + "/" + filename, outputfilename, int(self.entResolution.get()))
+            layerthickness = float(self.entLayerThickness.get())
+            resolution = int(1.0/layerthickness)
+            convert.doExport(self.inputdirname + "/" + filename, outputfilename, resolution)
         self.show_model()
 
     def show_model(self):
@@ -208,7 +260,7 @@ class MainWindow:
         dimesions.height = h
         dimesions.depth = count
         self.content.dimensions = dimesions
-
+        self.content.voxel = list()
         colors = np.array([[['#00000000']*w]*h]*count)
         for j in range(0,count):
 
@@ -233,7 +285,9 @@ class MainWindow:
                             counter=counter+1
                     if(colors[j][py][px] != '#00000000'):
                         voxel = Voxel()
-                        voxel.color = colors[j][py][px]
+                        hexcolor = matplotlib.colors.cnames[colors[j][py][px]].lstrip('#')
+                        rgbcolor = str(tuple(int(hexcolor[i:i+2], 16) for i in (0, 2 ,4)))
+                        voxel.color = rgbcolor
                         position = Position()
                         position.x = px
                         position.y = py
@@ -241,8 +295,8 @@ class MainWindow:
                         voxel.position = position 
                         self.content.voxel.append(voxel)
 
-        print("Total no.of Voxels:")
-        print(counter)
+        print("Total count of Voxels: %d", count * w * h)
+        print("Colored count of Voxels: %d", counter)
         filled = np.array(k)
         self.ax.voxels(filled,facecolors=colors, edgecolors='gray')
         self.canvas.draw()
@@ -256,10 +310,20 @@ class MainWindow:
         xml_doc = pyconvert.pyconv.convert2XML(self.content)
         # print(xml_doc.toprettyxml())
         print('xml generating...')
-        file = open(self.inputdirname + "/" + "voxeldata.xml", 'w')
+        outputdirectoryname = self.inputdirname + "/result"
+        if not os.path.exists(outputdirectoryname):
+            os.mkdir(outputdirectoryname)
+            print("Directory " , outputdirectoryname ,  " Created ")
+        file = open(outputdirectoryname + "/" + "voxeldata.xml", 'w')
         file.write(xml_doc.toprettyxml())
         file.close()
         print('xml generated')
+
+    def on_cancel(self, event):
+        self.inputdirname = ''
+        self.cboPartList['value']=''
+        self.ax.cla()
+        self.canvas.draw()
 
 if __name__ == '__main__':
     vp_start_gui()
